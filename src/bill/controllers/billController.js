f12b4adf27b96d94d8acb8de52dcc43a112b915f
bCuongIUH <bachcuong27@gmail.com>
2024-10-07 23:30:50 +0700

    create bill

--- src/bill/controllers/billController.js
+++ src/bill/controllers/billController.js
@@ -4,44 +4,47 @@ const Product = require('../../products/models/product');
 
 // Tạo hóa đơn từ giỏ hàng đã mua
 exports.createBill = async (req, res) => {
-    try {
-      const { userId, paymentMethod } = req.body;
-      const cart = await Cart.findOne({ user: userId, status: 'ChoThanhToan' }).populate('items.product');
-      if (!cart || cart.items.length === 0) {
-        return res.status(404).json({ message: 'Giỏ hàng trống hoặc không có sản phẩm nào để thanh toán' });
-      }
-      const totalAmount = cart.items.reduce((acc, item) => acc + item.totalPrice, 0);
-      // Tạo hóa đơn
-      const bill = new Bill({
-        user: userId,
-        items: cart.items,
-        totalAmount,
-        paymentMethod,
-      });
-      await bill.save();
-      // Cập nhật trạng thái giỏ hàng
-      cart.status = 'Shipped'; 
-      await cart.save();
-      
-      //5-10 fix lỗi giảm số lượng khi thanh toán
-      // Giảm số lượng sản phẩm tương ứng trong kho
-      for (const item of cart.items) {
-        const productLine = item.product.lines[0]; 
-        const newQuantity = productLine.quantity - item.quantity;
-  
-        if (newQuantity < 0) {
-          return res.status(400).json({ message: 'Số lượng sản phẩm trong kho không đủ' });
-        }
-  
-        productLine.quantity = newQuantity;
-        await item.product.save(); 
-      }
-  
-      res.status(201).json({ message: 'Hóa đơn đã được tạo thành công', bill });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    const { userId, paymentMethod } = req.body;
+    // console.log('userId:', userId, 'paymentMethod:', paymentMethod); 
+    const cart = await Cart.findOne({ user: userId, status: 'ChoThanhToan' }).populate('items.product');
+    // console.log('Giỏ hàng:', cart); 
+    if (!cart || cart.items.length === 0) {
+      return res.status(404).json({ message: 'Giỏ hàng trống hoặc không có sản phẩm nào để thanh toán' });
     }
-  };
+
+    const totalAmount = cart.items.reduce((acc, item) => acc + item.totalPrice, 0);
+    
+    const bill = new Bill({
+      user: userId,
+      items: cart.items,
+      totalAmount,
+      paymentMethod,
+    });
+    await bill.save();
+
+    cart.status = 'Shipped'; 
+    await cart.save();
+
+    for (const item of cart.items) {
+      const productLine = item.product.lines[0];
+      const newQuantity = productLine.quantity - item.quantity;
+  
+      if (newQuantity < 0) {
+        return res.status(400).json({ message: 'Số lượng sản phẩm trong kho không đủ' });
+      }
+  
+      productLine.quantity = newQuantity;
+      await item.product.save(); 
+    }
+  
+    res.status(201).json({ message: 'Hóa đơn đã được tạo thành công', bill });
+  } catch (error) {
+    console.error('Lỗi :', error); 
+    res.status(500).json({ message: error.message });
+  }
+};
+
 //mua hàng trục tiếp
 exports.createDirectPurchaseBill = async (req, res) => {
   try {
